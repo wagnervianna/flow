3caaf4c9fd86eb74976a24e4e88a406817efe139
Wagner Martins <wagnermartins@wagnermac.local>
2020-04-29 17:38:06 -0300

    first commi

---
+++ src/test/java/io/spring/batch/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
